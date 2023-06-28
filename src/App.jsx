b3693440b2ca148dd6ecebacd4dbde930d1e8308
MerlinConnected <gaetanjestin@outlook.com>
2023-06-28 09:11:58 +0200

    feat: Shadows, lights & first models

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber'
+import { EffectComposer, N8AO } from '@react-three/postprocessing'
+import { OrbitControls, Center, Gltf, Sky, BakeShadows } from '@react-three/drei'
 
 export default function App() {
-  return (
-    <Canvas>
-      <mesh>
-        <boxGeometry />
-        <meshBasicMaterial color='darkblue' />
-      </mesh>
-      <OrbitControls />
-    </Canvas>
-  );
+	return (
+		<Canvas shadows gl={{ antialias: false }} camera={{ position: [10, 10, 0] }}>
+			<color attach='background' args={['#dcdcdc']} />
+			<ambientLight intensity={0.2} />
+			<directionalLight color='white' intensity={2} angle={0.7} penumbra={5} position={[0, 20, 30]} castShadow shadow-mapSize={1024} shadow-bias={0} />
+
+			<OrbitControls />
+
+			<Center>
+				<boxGeometry args={[1, 1, 1]} color={'hotpink'} />
+				<Gltf castShadow receiveShadow scale={0.1} src='/docks.glb' inject={<meshStandardMaterial color='white' />} />
+			</Center>
+
+			<EffectComposer disableNormalPass multisampling={8}>
+				<N8AO aoRadius={50} distanceFalloff={0.3} intensity={6} screenSpaceRadius />
+			</EffectComposer>
+			<BakeShadows />
+
+			{/* <Sky /> */}
+		</Canvas>
+	)
 }
