7bd40e7fcd4910d75e6a59d8df07a628dd2a2a80
MerlinConnected <gaetanjestin@outlook.com>
2023-06-28 09:25:56 +0200

    fix: shadow map

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,36 @@ export default function App() {
 		<Canvas shadows gl={{ antialias: false }} camera={{ position: [10, 10, 0] }}>
 			<color attach='background' args={['#dcdcdc']} />
 			<ambientLight intensity={0.2} />
-			<directionalLight color='white' intensity={2} angle={0.7} penumbra={5} position={[0, 20, 30]} castShadow shadow-mapSize={1024} shadow-bias={0} />
+			<directionalLight
+				color='white'
+				intensity={2}
+				angle={0.2}
+				penumbra={1}
+				position={[10, 5, 10]}
+				castShadow
+				shadow-mapSize={1024}
+				shadow-bias={0}
+			>
+				<orthographicCamera attach='shadow-camera' args={[-15, 15, 10, -5, 1, 100]} />
+			</directionalLight>
 
 			<OrbitControls />
 
 			<Center>
 				<boxGeometry args={[1, 1, 1]} color={'hotpink'} />
-				<Gltf castShadow receiveShadow scale={0.1} src='/docks.glb' inject={<meshStandardMaterial color='white' />} />
+				<Gltf
+					castShadow
+					receiveShadow
+					scale={0.1}
+					src='/camargue.glb'
+					inject={<meshStandardMaterial color='white' />}
+				/>
 			</Center>
 
 			<EffectComposer disableNormalPass multisampling={8}>
 				<N8AO aoRadius={50} distanceFalloff={0.3} intensity={6} screenSpaceRadius />
 			</EffectComposer>
-			<BakeShadows />
+			{/* <BakeShadows /> */}
 
 			{/* <Sky /> */}
 		</Canvas>
